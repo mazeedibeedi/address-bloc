a1ad0813e3629493b97468ef25e7baa440d9ccc3
Ahmed Almazidi <a.almazidi92@gmail.com>
2017-05-19 16:51:49 +0300

    Assignment 4 Models completed

--- models/address_book.rb
+++ models/address_book.rb
@@ -17,4 +17,15 @@ class AddressBook
     end
     entries.insert(index, Entry.new(name, phone_number, email))
   end
+
+  def remove_entry(name, phone_number, email)
+    index = 0
+    entries.each do |entry|
+      if entry.to_s == "Name: #{name}\nPhone Number: #{phone_number}\nEmail: #{email}"
+        break
+      end
+      index += 1
+    end
+    entries.delete_at(index)
+  end
 end
