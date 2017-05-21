f17de7d34d7c1102bb4abe7060dc3e7226cb7296
Ahmed Almazidi <a.almazidi92@gmail.com>
2017-05-21 17:03:03 +0300

    Assignment 5 Menu complete

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -13,7 +13,8 @@ class MenuController
     puts "2 - Create an entry"
     puts "3 - Search fo an entry"
     puts "4 - Import entries from a CSV"
-    puts "5 - Exit"
+    puts "5 - View Entry Number n"
+    puts "6 - Exit"
     print "Enter your selection: "
 
     selection = gets.to_i
@@ -36,6 +37,10 @@ class MenuController
         read_csv
         main_menu
       when 5
+        system "clear"
+        specific_entry
+        main_menu
+      when 6
         puts "Good-bye!"
         exit(0)
       else
@@ -76,6 +81,18 @@ class MenuController
   end
 
   def read_csv
+  end
+
+  def specific_entry
+    print "Please enter the entry number: "
+
+    selection = gets.to_i
+    if !address_book.entries[selection - 1].nil?
+        puts address_book.entries[selection - 1].to_s
+    else
+        puts "Please enter a valid entry number"
+        specific_entry
+    end
   end
 
   def entry_submenu(entry)
